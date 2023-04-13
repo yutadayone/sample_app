e72dbc55b8ddab0d14462e05919b3817b8b84550
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-04-13 14:33:43 +0000

    5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -20,8 +20,15 @@ end
   end
 
   def edit
+    @list = List.find(params[:id])
   end
-
+  
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+    
   private
    # ストロングパラメータ
   def list_params
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   post 'lists' => 'lists#create'
    get 'lists' => 'lists#index'
 
-  get 'lists/edit'
   get '/top' => 'homes#top'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    # .../lists/1 や .../lists/3 に該当する
     get 'lists/:id' => 'lists#show', as: 'list'
+    get 'lists/:id/edit' => 'lists#edit', as: 'edit_list'
+    patch 'lists/:id' => 'lists#update', as: 'update_list'
 end
