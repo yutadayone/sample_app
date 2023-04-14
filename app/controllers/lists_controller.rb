9e6a10e696a75df42978fe899835a2d27cf0dfc1
yutadayone <129708410+yutadayone@users.noreply.github.com>
2023-04-14 07:44:56 +0000

    7

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -28,11 +28,17 @@ end
     list.update(list_params)
     redirect_to list_path(list.id)
   end
+  
+    def destroy
+      list = List.find(params[:id])
+      list.destroy
+      redirect_to '/lists'
+  end
     
   private
    # ストロングパラメータ
   def list_params
-    params.require(:list).permit(:title, :body)
+    params.require(:list).permit(:title, :body, :image)
   end
 
 end
